969faa2adcce2452440c8fbb7ed75f0570bdd21a
Laura Seidler <laura.seidler@gmx.de>
2018-02-23 18:59:31 +0100

    Enable ESLint for UI, make node_modules accessible locally

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,40 +5,36 @@ import './App.css';
 import { subscribeToTimer } from './api';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  state = {
+    message: '',
+    timestamp: 'no timestamp yet'
+  };
 
-        this.state = {
-            message: '',
-            timestamp: 'no timestamp yet',
-        };
+  componentDidMount() {
+    subscribeToTimer(1000, (err, timestamp) => this.setState({ timestamp }));
 
-        subscribeToTimer(1000, (err, timestamp) => this.setState({ timestamp }));
-    }
-
-    componentDidMount() {
-        return fetch('/api/hello')
-            .then(response => response.json())
-            .then(response => {
-                this.setState({
-                    message: response.message,
-                });
-            });
-    }
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    Message from API: <b>{this.state.message}</b> <br />
-                    This is the timer value: {this.state.timestamp}
-                </p>
-            </div>
-        );
-    }
+    return fetch('/api/hello')
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          message: response.message
+        });
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          Message from API: <b>{this.state.message}</b> <br />
+          This is the timer value: {this.state.timestamp}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
--- ui/src/api.js
+++ ui/src/api.js
@@ -3,8 +3,8 @@ import openSocket from 'socket.io-client';
 const socket = openSocket(`http://localhost:${process.env.WS_PORT || 4000}`);
 
 function subscribeToTimer(interval, cb) {
-    socket.on('timer', timestamp => cb(null, timestamp));
-    socket.emit('subscribeToTimer', interval);
+  socket.on('timer', timestamp => cb(null, timestamp));
+  socket.emit('subscribeToTimer', interval);
 }
 
 export { subscribeToTimer };
